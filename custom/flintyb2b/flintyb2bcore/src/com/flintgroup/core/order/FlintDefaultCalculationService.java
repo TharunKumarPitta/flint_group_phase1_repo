7a37fb8455f0ff7de82725e81cc043df170cb150
Tailor <629167@cognizant.com>
2017-07-11 15:08:15 +0530

    Added override findBasePrice method

---
+++ custom/flintyb2b/flintyb2bcore/src/com/flintgroup/core/order/FlintDefaultCalculationService.java
@@ -0,0 +1,47 @@
+/**
+ *
+ */
+package com.flintgroup.core.order;
+
+import de.hybris.platform.core.model.order.AbstractOrderEntryModel;
+import de.hybris.platform.order.exceptions.CalculationException;
+import de.hybris.platform.order.impl.DefaultCalculationService;
+import de.hybris.platform.order.strategies.calculation.FindPriceStrategy;
+import de.hybris.platform.util.PriceValue;
+
+
+/**
+ * @author 629167
+ *
+ */
+public class FlintDefaultCalculationService extends DefaultCalculationService
+{
+	private FindPriceStrategy findPriceStrategy;
+
+	/**
+	 * @return the findPriceStrategy
+	 */
+	public FindPriceStrategy getFindPriceStrategy()
+	{
+		return findPriceStrategy;
+	}
+
+	/**
+	 * @param findPriceStrategy
+	 *           the findPriceStrategy to set
+	 */
+	@Override
+	public void setFindPriceStrategy(final FindPriceStrategy findPriceStrategy)
+	{
+		this.findPriceStrategy = findPriceStrategy;
+	}
+
+
+
+	@Override
+	protected PriceValue findBasePrice(final AbstractOrderEntryModel entry) throws CalculationException
+	{
+		return entry.getBasePrice().equals(new Double(0.0)) ? new PriceValue(entry.getOrder().getCurrency().toString(), 0.0, true)
+				: getFindPriceStrategy().findBasePrice(entry);
+	}
+}
